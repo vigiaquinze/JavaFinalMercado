f0e8b264b6a6289fadb6b7f4465e57ed30d5f981
vigiaquinze <victorhugopagliuca84@gmail.com>
2023-12-07 16:36:02 -0300

    07/12 16h35

--- MercadoJavaSA4/src/View/EstoqueGUI.java
+++ MercadoJavaSA4/src/View/EstoqueGUI.java
@@ -1,26 +1,22 @@
 package View;
 
-import java.util.ArrayList;
+import javax.swing.*;
+import java.util.*;
 import java.util.List;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.*;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
 
 import Model.Estoque;
 import javafx.event.ActionEvent;
 import Control.EstoqueControl;
+import Connection.EstoqueDAO;
 
 public class EstoqueGUI extends JPanel {
     private JTextField inputIdProduto;
@@ -32,6 +28,7 @@ public class EstoqueGUI extends JPanel {
     private List<Estoque> estoque = new ArrayList<>();
     private int linhaSelecionada = -1;
     private JButton cadastrarButton, editarButton, apagarButton;
+
     public EstoqueGUI() {
         tableModel = new DefaultTableModel();
         tableModel.addColumn("ID");
@@ -40,7 +37,7 @@ public class EstoqueGUI extends JPanel {
         tableModel.addColumn("Valor Unitário");
         table = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(table);
-        //criando os métodos de entrada de dados (input)
+        // criando os métodos de entrada de dados (input)
         inputIdProduto = new JTextField(10);
         inputNomeProduto = new JTextField(20);
         inputQuantidade = new JTextField(10);
@@ -48,7 +45,7 @@ public class EstoqueGUI extends JPanel {
         cadastrarButton = new JButton("Cadastrar");
         editarButton = new JButton("Editar");
         apagarButton = new JButton("Apagar");
-        //adicionando os inputs
+        // adicionando os inputs
         JPanel inputPanel = new JPanel();
         inputPanel.add(new JLabel("ID:"));
         inputPanel.add(inputIdProduto);
@@ -65,7 +62,7 @@ public class EstoqueGUI extends JPanel {
         add(inputPanel, BorderLayout.NORTH);
         add(scrollPane, BorderLayout.CENTER);
 
-        //botões de eventos
+        // botões de eventos
 
         EstoqueControl operacoes = new EstoqueControl(estoque, tableModel, table);
 
@@ -81,5 +78,42 @@ public class EstoqueGUI extends JPanel {
                 }
             }
         });
+
+        cadastrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ev) {
+                operacoes.cadastrarProduto(inputIdProduto.getText(), inputNomeProduto.getText(),
+                        inputQuantidade.getText(), inputValorUnitario.getText());
+                inputIdProduto.setText("");
+                inputNomeProduto.setText("");
+                inputQuantidade.setText("");
+                inputValorUnitario.setText("");
+                JOptionPane.showMessageDialog(getComponentPopupMenu(), "Produto cadastrado.");
+            }
+        });
+
+        editarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ev) {
+                operacoes.editarProduto(linhaSelecionada, inputIdProduto.getText(), inputNomeProduto.getText(),
+                        inputQuantidade.getText(), inputValorUnitario.getText());
+                JOptionPane.showMessageDialog(getComponentPopupMenu(), "Produto editado.");
+            }
+        });
+
+        apagarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operacoes.apagarProduto(inputIdProduto.getText());
+                JOptionPane.showMessageDialog(getComponentPopupMenu(), "Produto removido.");
+            }
+        });
+    }
+    private void atualizarTabela() {
+        estoque = new EstoqueDAO().listarTodos();
+        tableModel.setRowCount(0);
+        for (Estoque estoque : estoque) {
+            tableModel.addRow(new Object[] { estoque.getIdProduto(), estoque.getNomeProduto(), estoque.getQuantidade(), estoque.getValorUnitario() });
+        }
     }
 }

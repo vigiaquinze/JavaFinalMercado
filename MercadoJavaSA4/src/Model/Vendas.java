97a1d12441d1740295226b0041806f1b3e00c692
Wallacy <wallacy.teixeira@outlook.com>
2023-12-07 13:57:49 -0300

    wallacy07/12/23

--- MercadoJavaSA4/src/Model/Vendas.java
+++ MercadoJavaSA4/src/Model/Vendas.java
@@ -4,28 +4,55 @@ public class Vendas {
     private String id;
     private String data;
     private String quantidadeVendida;
-    public Vendas(String id, String data, String quantidadeVendida) {
+    private boolean valor;
+    private int cpf;
+
+    public Vendas(String id, String data, String quantidadeVendida, boolean valor, int cpf) {
         this.id = id;
         this.data = data;
         this.quantidadeVendida = quantidadeVendida;
+        this.valor = valor;
+        this.cpf = cpf;
     }
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
+
     public String getData() {
         return data;
     }
+
     public void setData(String data) {
         this.data = data;
     }
+
     public String getQuantidadeVendida() {
         return quantidadeVendida;
     }
+
     public void setQuantidadeVendida(String quantidadeVendida) {
         this.quantidadeVendida = quantidadeVendida;
     }
-    
+
+    public boolean isValor() {
+        return valor;
+    }
+
+    public void setValor(boolean valor) {
+        this.valor = valor;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
 }
--- MercadoJavaSA4/src/View/ClientesGUI.java
+++ MercadoJavaSA4/src/View/ClientesGUI.java
@@ -1,5 +1,57 @@
 package View;
 
-public class ClientesGUI {
-    
+import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.GridLayout;
+
+import Model.Clientes;
+
+public class ClientesGUI extends JPanel {
+    private JButton cadastrar, apagar, editar;
+    private JTextField clintNome, clintCPF;
+    private List<Clientes> clientes;
+    private JTable table;
+    private DefaultTableModel tableModel;
+    private int linhaSelecionada = -1;
+
+    public ClientesGUI() {
+        super();
+        // entrada de dados
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        add(new JLabel("Histórico de Clientes"));
+        JPanel inputPanel = new JPanel();
+
+        inputPanel.setLayout(new GridLayout(5, 2));
+        inputPanel.add(new JLabel("Nome"));
+        clintNome = new JTextField(20);
+        inputPanel.add(clintNome);
+
+        inputPanel.setLayout(new GridLayout(5, 2));
+        inputPanel.add(new JLabel("Nome"));
+        clintCPF = new JTextField(20);
+        inputPanel.add(clintCPF);
+
+        add(inputPanel);
+        JPanel botoes = new JPanel();
+        botoes.add(cadastrar = new JButton("Cadastrar"));
+        botoes.add(editar = new JButton("Editar"));
+        botoes.add(apagar = new JButton("Apagar"));
+        add(botoes);
+        // tabela de clientes
+        JScrollPane jSPane = new JScrollPane();
+        add(jSPane);
+        tableModel = new DefaultTableModel(new Object[][] {},
+                new String[] { "Nome", "Endereço", "Contato", "CPF" });
+        table = new JTable(tableModel);
+        jSPane.setViewportView(table);
+    }
 }
